bde31f13737c347dbc452a5c07e92700b63bd12e
Daniel Blanco Parla <dblancoparla@gmail.com>
2021-07-12 23:53:05 +0200

    enhance ABI method detection

--- src/tx-decoder.ts
+++ src/tx-decoder.ts
@@ -1,26 +1,27 @@
-import { BigNumberish } from '@ethersproject/bignumber'
-import { defaultAbiCoder } from './types'
+import { BigNumberish } from "@ethersproject/bignumber";
+import { defaultAbiCoder } from "./types";
 
-import Decoder from './decoder'
+import Decoder from "./decoder";
 
 export default class TxDecoder extends Decoder {
-  decodeTx(tx: { data: string, value?: BigNumberish }) {
+  decodeTx(tx: { data: string; value?: BigNumberish }) {
     for (let i = 0; i < this.interfaces.length; i++) {
-      const iface = this.interfaces[i]
-      const parsedTransaction = iface.parseTransaction(tx)
+      const iface = this.interfaces[i];
+      const parsedTransaction = iface.parseTransaction(tx);
       if (parsedTransaction) {
         const parsedArgs = defaultAbiCoder.decode(
-          iface.functions[parsedTransaction.name].inputs,
+          iface.functions[parsedTransaction.name || parsedTransaction.signature]
+            .inputs,
           `0x${tx.data.substring(10)}`
-        )
+        );
 
         return {
           parsedArgs,
           ...parsedTransaction,
-        }
+        };
       }
     }
 
-    return null
+    return null;
   }
 }
